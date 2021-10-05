a56403340c2afbc7ec1d35706a6309627058ecb1
sushant2931 <sushantlad8888@gmail.com>
2021-10-05 23:58:28 +0530

    Added : Welcome In Address Book Program

---
+++ src/main/java/com/bridgelabz/addressbookservice/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookservice;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("..... Welcome In Address Book System Program Using JDBC");
+    }
+}
